080cc35008d9d40b562c1bca158780c38406f6bd
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-01-29 11:58:49 -0500

    Wrote migrations to create new tables

--- db/migrate/20220129164854_create_reviews.rb
+++ db/migrate/20220129164854_create_reviews.rb
@@ -1,4 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :activities_completed
+      t.string :comments
+      t.integer :rating
+      t.trip_id :integer
+      t.user_id :integer
+      t.timestamps
   end
 end
--- db/migrate/20220129164904_create_trips.rb
+++ db/migrate/20220129164904_create_trips.rb
@@ -1,4 +1,9 @@
 class CreateTrips < ActiveRecord::Migration[6.1]
   def change
+    create_table :trips do |t|
+      t.string :location
+      t.datetime :date_arrived
+      t.datetime :date_departed
+    end
   end
 end
