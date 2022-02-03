35e23f08fd8d141722a0255eb3150bcf583b8e53
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-02-02 19:13:48 -0500

    Updated routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
+  get "/trips" do
     trips = Trip.all
     trips.to_json
   end
